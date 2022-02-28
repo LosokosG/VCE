1ac7c52a06ed413d06a36b3e5315f8715edf0c0a
LosokosG <losokos@gmail.com>
2022-02-28 22:43:06 +0100

    n

--- src/main/java/losokos/vce/custom/CopperHelmetItem.java
+++ src/main/java/losokos/vce/custom/CopperHelmetItem.java
@@ -1,6 +1,7 @@
 package losokos.vce.custom;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EquipmentSlot;
@@ -10,10 +11,10 @@ import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraftforge.client.IBlockRenderProperties;
-import net.minecraftforge.common.extensions.IForgeBlockState;
-
-import java.util.concurrent.TimeUnit;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.phys.Vec3;
 
 public class CopperHelmetItem extends ArmorItem {
     public CopperHelmetItem(ArmorMaterial material, EquipmentSlot slot, Properties properties) {
@@ -24,11 +25,16 @@ public class CopperHelmetItem extends ArmorItem {
     public void onArmorTick(ItemStack stack, Level world, Player player) {
         if (!world.isClientSide()){
 
-
             if(world.isThundering()){
                 //Adds "lightningEntity" that spawns lightning at players location
                 Entity lightningEntity = new LightningBolt(EntityType.LIGHTNING_BOLT, world);
                 lightningEntity.setPos(player.getX(),player.getY(),player.getZ());
+
+                BlockPos posAbove = player.getPosition().up();
+                BlockState blockStateAbove = world.getBlockState(posAbove);
+                Block above = blockStateAbove.getBlock();
+
+                if(!above.equals(Material.AIR)){
 
                     //Spawns "lightningEntity" every x seconds
                     try {
@@ -38,12 +44,12 @@ public class CopperHelmetItem extends ArmorItem {
                     } catch (InterruptedException e) {
                         world.addFreshEntity(lightningEntity);
                     }
+                }
 
-
-
+                }
             }
         }
-    }}
+    }
 
     /*@Override
     public void onArmorTick(ItemStack stack, Level world, Player player) {
