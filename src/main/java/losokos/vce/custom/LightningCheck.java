4acd4644a7ebca9b3d85dfeb1bcee09ad51dcab0
losokos <losokos@gmail.com>
2022-03-10 17:57:33 +0100

    All armor pieces now repair on being struck by lightning while wearing.

--- src/main/java/losokos/vce/custom/LightningCheck.java
+++ src/main/java/losokos/vce/custom/LightningCheck.java
@@ -1,26 +1,7 @@
 package losokos.vce.custom;
 
-import com.mojang.math.Vector3f;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Position;
-import net.minecraft.core.Vec3i;
-import net.minecraft.network.protocol.game.ClientboundMoveEntityPacket;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LightningBolt;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.chunk.LevelChunk;
-import net.minecraft.world.level.gameevent.PositionSource;
-import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.Vec3;
-import org.jetbrains.annotations.Nullable;
-import org.w3c.dom.events.Event;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class LightningCheck {
     public static boolean LightningCheck(Player player){
